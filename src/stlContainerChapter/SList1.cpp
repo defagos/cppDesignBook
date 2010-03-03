b28e1cdaeb0a29703b64098bf7f20eaaed57580a
Samuel Defago <defagos@gmail.com>
2010-03-03 21:42:39 +0100

    Chapter about STL container creation added

---
+++ src/stlContainerChapter/SList1.cpp
@@ -0,0 +1,182 @@
+#include "SList1.h"
+
+#include <cassert>
+
+struct SList1::Node {
+  Node(const std::string &value, Node *pNextNode);
+
+  std::string m_value;
+  Node *m_pNextNode;
+};
+
+SList1::Node::Node(const std::string &value, Node *pNextNode)
+: m_value(value),
+  m_pNextNode(pNextNode)
+{}
+
+SList1::Iterator::Iterator()
+: m_pNode(0)
+{}
+
+SList1::Iterator &SList1::Iterator::operator++()
+{
+  m_pNode = m_pNode->m_pNextNode;
+  return *this;
+}
+
+const SList1::Iterator SList1::Iterator::operator++(int)
+{
+  Iterator tmp(*this);
+  m_pNode = m_pNode->m_pNextNode;
+  return tmp;
+}
+
+std::string *SList1::Iterator::operator->() const
+{
+  return &m_pNode->m_value;
+}
+
+std::string &SList1::Iterator::operator*() const
+{
+  return m_pNode->m_value;
+}
+
+bool operator==(const SList1::Iterator &lhs, const SList1::Iterator &rhs)
+{
+  return lhs.m_pNode == rhs.m_pNode;
+}
+
+bool operator!=(const SList1::Iterator &lhs, const SList1::Iterator &rhs)
+{
+  return lhs.m_pNode != rhs.m_pNode;
+}
+
+SList1::Iterator::Iterator(Node *pNode)
+: m_pNode(pNode)
+{}
+
+SList1::ConstIterator::ConstIterator()
+: m_pNode(0)
+{}
+
+SList1::ConstIterator::ConstIterator(const Iterator &rhs)
+: m_pNode(rhs.m_pNode)
+{}
+
+SList1::ConstIterator &SList1::ConstIterator::operator++()
+{
+  m_pNode = m_pNode->m_pNextNode;
+  return *this;
+}
+
+const SList1::ConstIterator SList1::ConstIterator::operator++(int)
+{
+  ConstIterator tmp(*this);
+  m_pNode = m_pNode->m_pNextNode;
+  return tmp;
+}
+
+const std::string *SList1::ConstIterator::operator->() const
+{
+  return &m_pNode->m_value;
+}
+
+const std::string &SList1::ConstIterator::operator*() const
+{
+  return m_pNode->m_value;
+}
+
+bool operator==(const SList1::ConstIterator &lhs, const SList1::ConstIterator &rhs)
+{
+  return lhs.m_pNode == rhs.m_pNode;
+}
+
+bool operator!=(const SList1::ConstIterator &lhs, const SList1::ConstIterator &rhs)
+{
+  return lhs.m_pNode != rhs.m_pNode;
+}
+
+SList1::ConstIterator::ConstIterator(const Node *pNode)
+: m_pNode(pNode)
+{}
+
+SList1::SList1()
+: m_pFirstNode(0)
+{}
+
+SList1::SList1(const SList1 &rhs)
+: m_pFirstNode(0)
+{
+  createFrom(rhs);
+}
+
+SList1 &SList1::operator=(const SList1 &rhs)
+{
+  if (this != &rhs) {
+    release();
+    createFrom(rhs);
+  }
+  return *this;
+}
+
+SList1::~SList1()
+{
+  release();
+}
+
+SList1::ConstIterator SList1::begin() const
+{
+  return ConstIterator(m_pFirstNode);
+}
+
+SList1::Iterator SList1::begin()
+{
+  return Iterator(m_pFirstNode);
+}
+
+SList1::ConstIterator SList1::end() const
+{
+  return ConstIterator(0);
+}
+
+SList1::Iterator SList1::end()
+{
+  return Iterator(0);
+}
+
+void SList1::push_front(const std::string &value)
+{
+  Node *pNode = new Node(value, m_pFirstNode);
+  m_pFirstNode = pNode;
+}
+
+void SList1::createFrom(const SList1 &rhs)
+{
+  // Can only create if current list is empty
+  assert(m_pFirstNode == 0);
+
+  Node *pRhsNode = rhs.m_pFirstNode;
+  Node *pNode = 0;
+  while (pRhsNode) {
+    if (! m_pFirstNode) {
+      m_pFirstNode = new Node(pRhsNode->m_value, 0);
+      pNode = m_pFirstNode;
+    }
+    else {
+      pNode->m_pNextNode = new Node(pRhsNode->m_value, 0);
+      pNode = pNode->m_pNextNode;
+    }
+    pRhsNode = pRhsNode->m_pNextNode;
+  }
+}
+
+void SList1::release()
+{
+  Node *pNode = m_pFirstNode;
+  while (pNode) {
+    Node *pNextNode = pNode->m_pNextNode;
+    delete pNode;
+    pNode = pNextNode;
+  }
+  m_pFirstNode = 0;
+}
---
+++ src/stlContainerChapter/SList1.h
@@ -0,0 +1,79 @@
+#ifndef SLIST1_H
+#define SLIST1_H
+
+#include <string>
+
+class SList1 {
+private:
+  struct Node;
+
+public:
+  class ConstIterator;
+
+  class Iterator {
+  public:
+    Iterator();
+
+    Iterator &operator++();
+    const Iterator operator++(int);
+
+    std::string *operator->() const;
+    std::string &operator*() const;
+
+    friend bool operator==(const Iterator &lhs, const Iterator &rhs);
+    friend bool operator!=(const Iterator &lhs, const Iterator &rhs);
+  
+  private:
+    friend class SList1;
+    friend class ConstIterator;
+
+    explicit Iterator(Node *pNode);
+
+    Node * m_pNode;
+  };
+
+  class ConstIterator {
+  public:
+    ConstIterator();
+    ConstIterator(const Iterator &rhs);
+
+    ConstIterator &operator++();
+    const ConstIterator operator++(int);
+
+    const std::string *operator->() const;
+    const std::string &operator*() const;
+
+    friend bool operator==(const ConstIterator &lhs, const ConstIterator &rhs);
+    friend bool operator!=(const ConstIterator &lhs, const ConstIterator &rhs);
+
+  private:
+    friend class SList1;
+
+    explicit ConstIterator(const Node *);
+
+    const Node * m_pNode;
+  };
+
+  SList1();
+  
+  SList1(const SList1 &rhs);
+  SList1 &operator=(const SList1 &rhs);
+
+  ~SList1();
+
+  ConstIterator begin() const;
+  Iterator begin();
+
+  ConstIterator end() const;
+  Iterator end();
+
+  void push_front(const std::string &value);
+
+private:
+  void createFrom(const SList1 &rhs);
+  void release();
+
+  Node *m_pFirstNode;
+};
+
+#endif
